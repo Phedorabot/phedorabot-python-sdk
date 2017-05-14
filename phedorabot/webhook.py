afeba0c4c3ab49af092c7c85e026d5ee98ab5706
Christian Amaonwu <ozala24@yahoo.com>
2017-05-14 21:55:34 +0100

    work on progress

--- phedorabot/webhook.py
+++ phedorabot/webhook.py
@@ -16,13 +16,58 @@
 
 from __future__ import absolute_import, division, print_function, with_statement
 
-from phedorabot.api import PhedorabotAPIClient
-from phedorabot import exceptions
+# from phedorabot.api import PhedorabotAPIClient
+# from phedorabot import exceptions
 
 import json
 import types
 import hmac
 import hashlib
+
+def flatten_payload(attrs, parent_key=None):
+    blobs = {}
+    if attrs is None or not type(attrs) in [types.DictType, types.ListType]:
+        return blobs
+
+    def value_type_check(value):
+        # Checks a type and returns valid
+        # string
+        if type(value) == types.StringType:
+            return str(value)
+        elif type(value) == types.UnicodeType:
+            value = value.encode('utf-8')
+            return str(value)
+        elif type(value) in [types.IntType, types.LongType]:
+            return str(value)
+        else:
+            return str(value) if type(value) == types.NoneType else ''
+
+    if type(attrs) == types.ListType:
+        for i,val in enumerate(attrs):
+            k = compute_valid_key(i, parent_key)
+            if type(val) in [types.DictType,types.ListType]:
+                children = flatten_payload(val, k)
+                for m1,n1 in children.iteritems():
+                    blobs[m1] = value_type_check(n1)
+            else:
+                blobs[k] = value_type_check(val)
+
+    if type(attrs) == types.DictType:
+        for i,j in attrs.iteritems():
+            k = compute_valid_key(i, parent_key)
+            if type(j) in [types.DictType, types.ListType]:
+                children = flatten_payload(j, k)
+                for m1,n1 in children.iteritems():
+                    blobs[m1] = value_type_check(n1)
+
+    return blobs
+
+def compute_valid_key(current_key, parent_key=None):
+    if parent_key is not None:
+        return '{0}_{1}'.format(str(current_key), str(parent_key))
+    else:
+        return str(current_key)
+
 
 class PhedorabotWebHookException(Exception):
     # The exception error
@@ -54,3 +99,9 @@ class PhedorabotWebHook(object):
         self.result = None
         self.body = None
         self.rawHeaderDict = {}
+
+
+if __name__ == '__main__':
+
+    maps = [{'name':'Christian','Age':'38'},{'name':'Vikky','age':'36'}]
+    print(flatten_payload(maps))
